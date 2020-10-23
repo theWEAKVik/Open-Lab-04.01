c4bf9a3ab59050bd78683af329bbed143b0de559
Viktor <krpectv57@gmail.com>
2020-10-23 19:17:06 +0200

    _

--- Open-Lab-04.01/Checker.cs
+++ Open-Lab-04.01/Checker.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
+using System.Linq;
 
 namespace Open_Lab_04._01
 {
@@ -6,7 +8,13 @@ namespace Open_Lab_04._01
     {
         public bool DoubleLetters(string str)
         {
-            throw new NotImplementedException();
+            int L = str.Length;                       
+            for (int i = 0; i < L-1; i++)
+            {               
+                if (i < L -1 && str[i] == str[i + 1])
+                    return true;                              
+            }
+            return false;
         }
     }
 }
